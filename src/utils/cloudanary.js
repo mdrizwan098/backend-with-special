60e45ce6e8d145a692214ece0f06ca763710c354
Rijjy <mdrizwan808411@gmail.com>
2026-01-09 21:01:45 +0530

    multer middleware

---
+++ src/utils/cloudanary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudnary } from "cloudinary"
+import { response } from "express";
+import fs from "fs"
+
+cloudnary.config({
+    cloud_name: 'process.env.CLOUDNARY_CLOUD_NAME',
+    api_key: 'process.env.CLOUDNARY_API_KEY',
+    api_secret: 'process.env.CLOUDNARY_CLOUD_NAME'
+});
+
+const uploadCloudnary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+
+        // upload the file on cloudnary
+        const response = await cloudnary.uploader.upload(localFilePath, { resource_type: "auto" })
+
+
+        //file has been uploaded sucessfully
+
+        console.log("file is uploaded on cloudanry",
+            response.url
+        );
+        return response;
+    } catch (error) {
+fs.unlinkSync(localFilePath) // rremove the locally saved temporarily file as the upload  operation got failed
+return null;
+    }
+}
+
+
+
+export {uploadCloudnary}
